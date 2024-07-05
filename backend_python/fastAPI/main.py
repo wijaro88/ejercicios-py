25facfc2ce7a146767598c0daec3cc3ab9b32a6b
Wilson Rocha <wijaro88@gmail.com>
2024-07-04 23:32:37 -0500

    act users.py

--- backend_python/fastAPI/main.py
+++ backend_python/fastAPI/main.py
@@ -12,8 +12,9 @@ from typing import Union
 app =FastAPI()
 
 #url local: http://127.0.0.1:8000
+
 @app.get('/')
-def read_root():
+async def read_root():
     return {"¡Hola FastApi!"}
 
 #url local: http://127.0.0.1:8000/items/{item_id}
@@ -23,7 +24,7 @@ async def read_item(item_id:int, q: Union[str, None]= None):
     return {"item_id":item_id, "q":q}    
 
 # Inicia el server: uvicorn main:app --reload
-# Destener el server: CTRL + C
+# Detener el server: CTRL + C
 
 # Documentación con Swagger: http://127.0.0.1:8000/docs
 # Documentación con Redocly: http://127.0.0.1:8000/redoc
--- backend_python/fastAPI/users.py
+++ backend_python/fastAPI/users.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from typing import Union
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/users')
-async def read_users():
-    return {"¡Hola Users!"}
+# Inicia el server: uvicorn users:app --reload
+
+# Entidad user
+
+#baseModel nos deja crear el objeto 
+class User(BaseModel):
+    name:str
+    surname:str
+    url:str
+    age:int
+
+users_list = [User(name="wilson",surname="javier",url="https://wilss.com",age=33),
+         User(name="wilson33",surname="javier",url="https://wilss.com",age=34),
+         User(name="wilson44",surname="javier",url="https://wilss.com",age=35)]
+
+
+@app.get('/usersjson')
+async def usersjson():
+    return [{"name":"wilson", "surname":"javier","url":"https://wilss.com","age":33},      
+            {"name":"wilson2", "surname":"javier2","url":"https://wilss3.com","age":34},
+            {"name":"wilson3", "surname":"javier3","url":"https://wilss2.com","age":35}]
+
+
+@app.get("/users")
+async def users():
+    return users_list
